ddbe49d64755785b2edcdebdcbed93abd7254939
minju <40803mj@kookmin.ac.kr>
2023-02-09 19:20:54 +0900

    1주차 과제 해결

--- src/pages/Week1/index.js
+++ src/pages/Week1/index.js
@@ -4,30 +4,48 @@ import { useState } from 'react';
 export default function Week1() {
   // 아래에 content 라는 state를 useState 함수를 사용하여 선언하시오.
   // const ...
+  const [content, setContent] = useState('내용을 입력하세요');
+
   const [textStyle, setTextStyle] = useState({
-    bold: true,
+    bold: false,
     italic: false,
     color: false,
   });
+  const {bold, italic, color} = textStyle;
+
 
   const handleCheckbox = e => {
     // 이 함수는 checkbox가 변하면 이벤트를 받아 처리해주는 함수이다.
     // 아래 힌트를 참고하여 체크박스가 변할 때 마다 textStyle state 값이 변하도록 하시오.
     // 브라우저에서 개발자 도구 - Console 에서 값을 확인할 수 있다.
+    const {name, checked} = e.target;
+    
+    setTextStyle({
+      ...textStyle,
+      [name]: checked
+    });
+
     console.log(e.target.name, e.target.checked);
   };
 
+    
+  
+
   // 아래 함수를 작성하여 input의 값이 변할 때, content state 값이 변하도록 하시오.
-  const handleInputText = e => {};
+  const handleInputText = e => {
+    setContent(e.target.value);
+  };
 
   return (
     <div style={{ padding: '20px' }}>
-      <input type="text" placeholder="내용을 입력하세요." /> <br />
+      <input type="text" value={content} onChange={handleInputText} placeholder="내용을 입력하세요." /> <br />
+      {/* <input handleInputText={content} value={content}  /> <br /> */}
+
       <input
         type="checkbox"
         id="bold"
         name="bold"
-        checked
+        // checked 굵기 조절할 수 있게 고정에서 수정
         onChange={handleCheckbox}
       />
       <label htmlFor="bold">
@@ -44,7 +62,7 @@ export default function Week1() {
       </label>
       <input
         type="checkbox"
-        id="color"
+        id="color" 
         name="color"
         onChange={handleCheckbox}
       />
@@ -53,11 +71,13 @@ export default function Week1() {
       </label>
       {/* 아래 StyledH2 Component는 각각 bold, italic, color을 props로 받아 적용시킨다. */}
       {/* 위에서 선언한 textStyle state 값을 props로 넘겨주도록 한다. */}
-      <StyledH2 bold={true}>
+      <StyledH2 bold={bold} italic={italic} color={color}>
         {
           // content state에 값이 없다면, '내용을 입력하세요.' 를 출력하고 값이 있다면 content state 값을 출력하시오.
+          content
         }
-        내용을 입력하세요.
+      
+
       </StyledH2>
     </div>
   );
