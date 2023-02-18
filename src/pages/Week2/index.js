e1a680931ee6814bc0bfcd2d94ce27217582412b
easyandones <easyandones@gmail.com>
2023-02-18 15:39:08 +0900

    2주차 과제 해결

--- src/pages/Week2/index.js
+++ src/pages/Week2/index.js
@@ -27,14 +27,16 @@ export default function Week2() {
   // 아래 미리 작성된 useEffect 함수를 참고하여 정상적으로 남은 시간이 돌아가도록 작성하시오.
   useEffect(() => {
     timer.current = setInterval(() => {
-      console.log('1초마다 실행');
+      setTime((time) => time - 1);
     }, 1000);
 
     return () => clearInterval(timer.current);
-  }, [])
+  }, []);
 
   useEffect(() => {
-    // time이 변할 때 마다 실행됨.
+    if (time === 0) {
+      clearInterval(timer.current);
+    }
   }, [time]);
 
   const handleRegister = (e) => {
@@ -43,13 +45,9 @@ export default function Week2() {
       return;
     }
     // e.target.name을 이용하여 alert 함수를 이용해 "(과목명) 수강신청에 성공하였습니다!" 가 출력되도록 하시오.
-    if (e.target.name === 'index') { // 단, 다음 코드처럼 if-else 구문으로 처리하지 마시오. (관련없는 예시 코드입니다.)
-      // e.target.name === '0' 과 같은 코드로 제출하시면 혼냅니다...
-      alert('알고리즘 과목은 수강인원이 초과되었습니다.');
-      return;
-    }
     // 이 아랫줄에 해당 코드를 작성하세요.
-  }
+    alert(`${e.target.name} 수강신청에 성공하였습니다!`);
+  };
 
   return (
     <div style={{ padding: '20px' }}>
@@ -66,12 +64,16 @@ export default function Week2() {
           </tr>
           {/* map 함수를 이용하여 timetable에 담긴 과목이 아래 예시와 같은 수강신청 화면으로 렌더링되게 하시오. */}
           {/* <tr> 태그에 key 값으로는 과목 내용에서 name으로 설정하시오. */}
-          <tr>
-            <td>알고리즘</td>
-            <td>최준호</td>
-            <td>15:00 ~ 16:00</td>
-            <td><button name="index" onClick={handleRegister}>신청</button></td>
-          </tr>
+          {
+            timetable.map(subject =>
+              <tr key={subject.name}>
+                <td>{subject.title}</td>
+                <td>{subject.professor}</td>
+                <td>{subject.time}</td>
+                <td><button name={subject.title} onClick={handleRegister}>신청</button></td>
+              </tr>
+            )
+          }
           {/* Hint! { timetable.map(subject => <tr key={subject.name}> ... */}
         </tbody>
       </table>
